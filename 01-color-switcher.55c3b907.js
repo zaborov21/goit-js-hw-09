fe9ea764db02fbaa6a8701b9f60ea2a478013a23
zaborov21 <104018173+zaborov21@users.noreply.github.com>
2023-09-15 19:54:42 +0000

    Deploying to gh-pages from @ zaborov21/goit-js-hw-09@71be7dbd015f3360a2cf29216217dd0e6e122ad4 🚀

--- 01-color-switcher.55c3b907.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=0;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,n.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.55c3b907.js.map
